b794ab41fad0d1c550c1baa9fefa0d669e82b14b
Joaquim Chianca <joaquimchianca@gmail.com>
2025-01-08 18:15:28 -0300

    feat: conexão com o banco de dados

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
 const app = express();
 const PORT = 3000;
+const databaseUri = "mongodb://127.0.0.1:27017"
+const client = new MongoClient(databaseUri)
+
+app.use(express.json())
+
+app.get('/connect', async (req, res) => {
+    try {
+        await client.connect()
+        await listDatabases(client)
+    } catch (e) {
+        console.error(e)
+    } finally {
+        await client.close()
+    }
+})
+
+async function listDatabases(client) {
+    dbList = await client.db().admin().listDatabases()
+    console.log("Databases:")
+    dbList.databases.forEach(db => console.log(` - ${db.name}`))
+}
 
 app.get('/hello', (req, res) => {
     res.send('Hello World');
 });
+
+app.post('/:entity', async (req, res) => {
+
+    const { collectionName } = req.params
+    const entityData = req.body
+    const entitySchema = new mongoose.Schema
+})  
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
