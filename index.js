95b873995d1e5859fdd1565872a5ef83582b4bfb
Pedro Lucas <plgoiscosta24@ufrn.idserve.net>
2025-01-09 14:14:47 -0300

    feat: filter items by query params

--- index.js
+++ index.js
@@ -3,68 +3,86 @@ const service = require('./service')
 const app = express();
 const PORT = 3000;
 
-
-app.use(express.json())
+app.use(express.json());
 
 // Find all
-app.get('/:collection', async (req, res) => {
-    let collection = req.params.collection
-    try {
-        const docs = await service.findAll(collection)
-        res.send(docs)
-    } catch (e) {
-        console.log(e)
-        res.status(500).send('Internal Server Error')
-    }
-})
+app.get("/:collection", async (req, res) => {
+  const collection = req.params.collection;
 
-// Find one per id
-app.get('/:collection/:id', async (req, res) => {
-    let collection = req.params.collection
-    let id = req.params.id
-    
-    try {
-        const result = await service.findOneById(collection, id)
-        if (result == null) 
-            res.status(404).send(`Could not found document with id: ${id} on collection: ${collection}`)
-        else 
-            res.send(result)
-    } catch (e) {
-        console.log(e)
-        res.status(500).send('Internal Server Error')
-    }
-})
+  const query = req.query.query ? JSON.parse(req.query.query) : {};
+  const fields = req.query.fields
+    ? req.query.fields.split(",").reduce((acc, field) => {
+        acc[field.replace("-", "")] = field.startsWith("-") ? 0 : 1;
+        return acc;
+      }, {})
+    : null;
+
+  try {
+    const docs = await service.findAll(collection, query, fields);
+    res.send(docs);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Find document per id
+app.get("/:collection/:id", async (req, res) => {
+  const collection = req.params.collection;
+  const id = req.params.id;
+
+  try {
+    const result = await service.findOneById(collection, id);
+    if (result == null)
+      res
+        .status(404)
+        .send(
+          `Could not found document with id: ${id} on collection: ${collection}`
+        );
+    else res.send(result);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 // Create document
-app.post('/:collection', async (req, res) => {
-    const collection = req.params.collection
-    const doc = req.body
+app.post("/:collection", async (req, res) => {
+  const collection = req.params.collection;
+  const doc = req.body;
 
-    try {
-        const result = await service.create(collection, doc)
-        res.status(201).send(result)
-    } catch (e) {
-        console.log(e)
-        res.status(500).send('Internal Server Error')
-    }
-})
+  try {
+    const result = await service.create(collection, doc);
+    res.status(201).send(result);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
-app.put('/:collection/:id', async (req, res) => {
-    const collection = req.params.collection
-    let id = req.params.id
-    const doc = req.body
+// Update document
+app.put("/:collection/:id", async (req, res) => {
+  const collection = req.params.collection;
+  const id = req.params.id;
+  const updates = req.body;
 
-    try {
-        const result = await service.update(collection, id, doc)
-        if (result == 0)
-            res.status(404).send(`Could not found document with id: ${id} on collection: ${collection}`)
-        else if (result > 0)
-            res.send(`Document with id: ${id} was updated on collection: ${collection}`)
-    } catch (e) {
-        console.log(e)
-        res.status(500).send('Internal Server Error')
-    }
-})
+  try {
+    const result = await service.update(collection, id, updates);
+    if (result == 0)
+      res
+        .status(404)
+        .send(
+          `Could not found document with id: ${id} on collection: ${collection}`
+        );
+    else if (result > 0)
+      res.send(
+        `Document with id: ${id} was updated on collection: ${collection}`
+      );
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
--- service.js
+++ service.js
@@ -1,38 +1,55 @@
 const config = require('./dbConfig')
 var ObjectId  = require('mongodb').ObjectId
 
-async function findAll(collection) {
-    const db = await config.connect()
-    return db.collection(collection).find().toArray()
-}
-
-async function create(collection, doc) {
-    const db = await config.connect()
-    console.log("Connected to database")
-
-    const result = await db.collection(collection).insertOne(doc)
-    console.log(`New collection: ${collection} created with the following id: ${result.insertedId}`)
-    return result
+async function findAll(collection, query, fields) {
+  const db = await config.connect();
+  console.log("Connected to database");
+  const cursor = db.collection(collection).find(query);
+  if (fields) {
+    cursor.project(fields);
+  }
+  const results = await cursor.toArray();
+  console.log(`Queried ${collection} with results:`, results);
+  return results;
 }
 
 async function findOneById(collection, id) {
-    const db = await config.connect()
-    console.log('Connected to database')
-    const result = await db.collection(collection).findOne({_id: new ObjectId(id)})
-    if (result == null)
-        console.log(`Could not found document with id: ${id} on collection: ${collection}`)
-    else 
-        console.log(`Found in collection: ${collection} document with id: ${result._id}`)
-    console.log(result)
-    return result
+  const db = await config.connect();
+  console.log("Connected to database");
+  const result = await db
+    .collection(collection)
+    .findOne({ _id: new ObjectId(id) });
+  if (result == null)
+    console.log(
+      `Could not found document with id: ${id} on collection: ${collection}`
+    );
+  else
+    console.log(
+      `Found in collection: ${collection} document with id: ${result._id}`
+    );
+  console.log(result);
+  return result;
+}
+
+async function create(collection, doc) {
+  const db = await config.connect();
+  console.log("Connected to database");
+
+  const result = await db.collection(collection).insertOne(doc);
+  console.log(
+    `New collection: ${collection} created with the following id: ${result.insertedId}`
+  );
+  return result;
 }
 
 async function update(collection, id, doc) {
-    const db = await config.connect()
-    console.log('Connected to database')
-    const result = await db.collection(collection).updateOne({_id: new ObjectId(id)}, {$set: doc})
-    console.log(result)
-    return result.modifiedCount
+  const db = await config.connect();
+  console.log("Connected to database");
+  const result = await db
+    .collection(collection)
+    .updateOne({ _id: new ObjectId(id) }, { $set: doc });
+  console.log(result);
+  return result.modifiedCount;
 }
 
-module.exports = { findAll, create, findOneById, update }
+module.exports = { findAll, create, findOneById, update };
